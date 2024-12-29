a0600a7bd6878ab698dbdfc80f24836b19a06e25
kalaiyarasanS296 <arasankalaiy820@gmail.com>
2024-12-29 12:45:32 +0530

    change in practice

--- AA_PRATICE/pravtice.java
+++ AA_PRATICE/pravtice.java
@@ -2,6 +2,6 @@ package AA_PRATICE;
 
 public class pravtice {
     public static void main(String[] args) {
-        System.out.println("kalai");
+        System.out.println("kalaxzcvbi");
     }
 }
